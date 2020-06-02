80d00b3faee1968c9a382b858d0b92c3d57c989f
MykolaV <kolya.vasevich@gmail.com>
2020-06-03 02:51:41 +0300

    added migration

---
+++ DAL/Migrations/20200602221434_InitialCreate.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dal.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                "AspNetRoles",
+                table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_AspNetRoles", x => x.Id); });
+
+            migrationBuilder.CreateTable(
+                "AspNetUsers",
+                table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_AspNetUsers", x => x.Id); });
+
+            migrationBuilder.CreateTable(
+                "Owners",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true)
+                },
+                constraints: table => { table.PrimaryKey("PK_Owners", x => x.Id); });
+
+            migrationBuilder.CreateTable(
+                "Parts",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ManufactureDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table => { table.PrimaryKey("PK_Parts", x => x.Id); });
+
+            migrationBuilder.CreateTable(
+                "AspNetRoleClaims",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        x => x.RoleId,
+                        "AspNetRoles",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "AspNetUserClaims",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        x => x.UserId,
+                        "AspNetUsers",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "AspNetUserLogins",
+                table => new
+                {
+                    LoginProvider = table.Column<string>(nullable: false),
+                    ProviderKey = table.Column<string>(nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new {x.LoginProvider, x.ProviderKey});
+                    table.ForeignKey(
+                        "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        x => x.UserId,
+                        "AspNetUsers",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "AspNetUserRoles",
+                table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    RoleId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new {x.UserId, x.RoleId});
+                    table.ForeignKey(
+                        "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        x => x.RoleId,
+                        "AspNetRoles",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        x => x.UserId,
+                        "AspNetUsers",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "AspNetUserTokens",
+                table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    LoginProvider = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new {x.UserId, x.LoginProvider, x.Name});
+                    table.ForeignKey(
+                        "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        x => x.UserId,
+                        "AspNetUsers",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "Computers",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                    table.ForeignKey(
+                        "FK_Computers_Owners_OwnerId",
+                        x => x.OwnerId,
+                        "Owners",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "Orders",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    OwnerId = table.Column<int>(nullable: false),
+                    RepairPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        "FK_Orders_Owners_OwnerId",
+                        x => x.OwnerId,
+                        "Owners",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "ComputerParts",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartId = table.Column<int>(nullable: false),
+                    ComputerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComputerParts", x => x.Id);
+                    table.ForeignKey(
+                        "FK_ComputerParts_Computers_ComputerId",
+                        x => x.ComputerId,
+                        "Computers",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_ComputerParts_Parts_PartId",
+                        x => x.PartId,
+                        "Parts",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                "OrderParts",
+                table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderParts", x => x.Id);
+                    table.ForeignKey(
+                        "FK_OrderParts_Orders_OrderId",
+                        x => x.OrderId,
+                        "Orders",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        "FK_OrderParts_Parts_PartId",
+                        x => x.PartId,
+                        "Parts",
+                        "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                "Owners",
+                new[] {"Id", "FirstName", "LastName", "SecondName"},
+                new object[,]
+                {
+                    {1, "Vasia", "Pupkin", "Yaroslavovych"},
+                    {2, "Sample", "User", "#2"}
+                });
+
+            migrationBuilder.InsertData(
+                "Parts",
+                new[] {"Id", "ManufactureDate", "Name"},
+                new object[,]
+                {
+                    {1, new DateTime(2020, 5, 24, 1, 14, 18, 406, DateTimeKind.Local).AddTicks(7658), "Sample part"},
+                    {2, new DateTime(2020, 5, 25, 1, 14, 18, 406, DateTimeKind.Local).AddTicks(8362), "Sample part #2"},
+                    {3, new DateTime(2020, 5, 26, 1, 14, 18, 406, DateTimeKind.Local).AddTicks(8402), "Sample part #3"}
+                });
+
+            migrationBuilder.InsertData(
+                "Computers",
+                new[] {"Id", "Model", "Name", "OwnerId"},
+                new object[,]
+                {
+                    {1, "abc-123", "Sample PC", 1},
+                    {2, "abc-124", "Sample PC", 2}
+                });
+
+            migrationBuilder.InsertData(
+                "Orders",
+                new[] {"Id", "CreationDate", "OwnerId", "RepairPrice"},
+                new object[,]
+                {
+                    {1, new DateTime(2020, 6, 3, 1, 14, 18, 403, DateTimeKind.Local).AddTicks(9096), 1, 9999},
+                    {2, new DateTime(2020, 6, 3, 1, 14, 18, 406, DateTimeKind.Local).AddTicks(7202), 2, 999}
+                });
+
+            migrationBuilder.InsertData(
+                "ComputerParts",
+                new[] {"Id", "ComputerId", "PartId"},
+                new object[,]
+                {
+                    {1, 1, 1},
+                    {3, 1, 2},
+                    {2, 2, 1},
+                    {4, 2, 3}
+                });
+
+            migrationBuilder.InsertData(
+                "OrderParts",
+                new[] {"Id", "OrderId", "PartId"},
+                new object[,]
+                {
+                    {1, 1, 1},
+                    {2, 2, 2}
+                });
+
+            migrationBuilder.CreateIndex(
+                "IX_AspNetRoleClaims_RoleId",
+                "AspNetRoleClaims",
+                "RoleId");
+
+            migrationBuilder.CreateIndex(
+                "RoleNameIndex",
+                "AspNetRoles",
+                "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                "IX_AspNetUserClaims_UserId",
+                "AspNetUserClaims",
+                "UserId");
+
+            migrationBuilder.CreateIndex(
+                "IX_AspNetUserLogins_UserId",
+                "AspNetUserLogins",
+                "UserId");
+
+            migrationBuilder.CreateIndex(
+                "IX_AspNetUserRoles_RoleId",
+                "AspNetUserRoles",
+                "RoleId");
+
+            migrationBuilder.CreateIndex(
+                "EmailIndex",
+                "AspNetUsers",
+                "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                "UserNameIndex",
+                "AspNetUsers",
+                "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                "IX_ComputerParts_ComputerId",
+                "ComputerParts",
+                "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                "IX_ComputerParts_PartId",
+                "ComputerParts",
+                "PartId");
+
+            migrationBuilder.CreateIndex(
+                "IX_Computers_OwnerId",
+                "Computers",
+                "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                "IX_OrderParts_OrderId",
+                "OrderParts",
+                "OrderId");
+
+            migrationBuilder.CreateIndex(
+                "IX_OrderParts_PartId",
+                "OrderParts",
+                "PartId");
+
+            migrationBuilder.CreateIndex(
+                "IX_Orders_OwnerId",
+                "Orders",
+                "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                "ComputerParts");
+
+            migrationBuilder.DropTable(
+                "OrderParts");
+
+            migrationBuilder.DropTable(
+                "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                "Computers");
+
+            migrationBuilder.DropTable(
+                "Orders");
+
+            migrationBuilder.DropTable(
+                "Parts");
+
+            migrationBuilder.DropTable(
+                "Owners");
+        }
+    }
+}
